327700ff1f76c18975ed4809f69388a2cfb0806f
Simon Fletcher <simonfletcher0@gmail.com>
2017-05-08 16:45:11 +0100

    refactoring main.go file to be more readable

--- 08-may-17/solutions/simon/main.go
+++ 08-may-17/solutions/simon/main.go
@@ -3,51 +3,63 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"strings"
 )
 
 func main() {
-	list, _ := file_to_list()
-	answer := runner(list)
-	fmt.Printf("%s", answer)
+	list, err := file_to_list()
+	if err != nil {
+		fmt.Printf("something went wrong, could not open file.")
+	}
+	fmt.Printf("%s", find_word(list))
 }
 
 func file_to_list() ([]string, error) {
 	file, err := ioutil.ReadFile("../../input.txt")
 	if err != nil {
-		log.Fatal("something went wrong, could not open file.")
+		return make([]string, 0), err
 	}
 	bytes_to_string := string(file)
 	return strings.Split(bytes_to_string, "\n"), nil
 }
 
-func runner(list []string) string {
+func find_word(lines []string) string {
 	acc := make(map[int]map[string]int)
-	for _, l := range list {
-		for i, v := range strings.Split(l, "") {
-			if len(acc[i]) == 0 {
-				acc[i] = make(map[string]int)
-			}
-			acc[i][v] = acc[i][v] + 1
-		}
+	for _, word := range lines {
+		// could probably use pointers here to get around re-assignment
+		acc = split_and_count(word, acc)
 	}
 	slice := sort_and_format(acc)
 	return strings.Join(slice, "")
 }
 
+func split_and_count(str string, acc map[int]map[string]int) map[int]map[string]int {
+	for i, v := range strings.Split(str, "") {
+		if len(acc[i]) == 0 {
+			acc[i] = make(map[string]int)
+		}
+		acc[i][v] = acc[i][v] + 1
+	}
+	return acc
+}
+
 func sort_and_format(map_of_maps map[int]map[string]int) []string {
 	var char_map []string
 	for i := 0; i <= len(map_of_maps); i++ {
-		var highest_character string
-		var last_highest_value int
-		for key, value := range map_of_maps[i] {
-			if last_highest_value < value {
-				highest_character = key
-				last_highest_value = value
-			}
-		}
-		char_map = append(char_map, highest_character)
+		char_map = append(char_map, highest_char_in_map(map_of_maps, i))
 	}
 	return char_map
+}
+
+func highest_char_in_map(map_of_maps map[int]map[string]int, i int) string {
+	var highest_character string
+	var last_highest_value int
+
+	for key, value := range map_of_maps[i] {
+		if last_highest_value < value {
+			highest_character = key
+			last_highest_value = value
+		}
+	}
+	return highest_character
 }
