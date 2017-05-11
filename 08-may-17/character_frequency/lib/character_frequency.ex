911fb7bd8014ff0d7346c986fdee4e993f69b206
Ed Ellson <eellson@gmail.com>
2017-05-11 10:49:25 +0100

    Init mix project

---
+++ 08-may-17/character_frequency/lib/character_frequency.ex
@@ -0,0 +1,18 @@
+defmodule CharacterFrequency do
+  @moduledoc """
+  Documentation for CharacterFrequency.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CharacterFrequency.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 08-may-17/character_frequency/test/character_frequency_test.exs
@@ -0,0 +1,8 @@
+defmodule CharacterFrequencyTest do
+  use ExUnit.Case
+  doctest CharacterFrequency
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
