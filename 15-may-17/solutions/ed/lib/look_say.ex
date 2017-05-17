d5ac2b78d3f49cff56fb3739b4a901957cb8d767
Ed Ellson <eellson@gmail.com>
2017-05-17 13:24:14 +0100

    Add alternative implementation
    This adds an implementation which uses binary pattern matching, as
    opposed to charlist

--- 15-may-17/solutions/ed/lib/look_say.ex
+++ 15-may-17/solutions/ed/lib/look_say.ex
@@ -3,24 +3,49 @@ defmodule LookSay do
   Documentation for LookSay.
   """
 
+  def charlist_solve do
+    charlist_parse('111312211382399229999992222441191239988800899922231114444111888223332222999988222288822211177281')
+  end
+
+  def binary_solve do
+    binary_parse("111312211382399229999992222441191239988800899922231114444111888223332222999988222288822211177281")
+  end
+
   @doc """
   Parses given input, returning next member of look and say sequence.
 
   ## Examples
 
-      iex> LookSay.parse('1')
+      iex> LookSay.charlist_parse('1')
       '11'
 
   """
-  def solve do
-    parse('111312211382399229999992222441191239988800899922231114444111888223332222999988222288822211177281')
-  end
+  @spec charlist_parse(input :: charlist()) :: charlist()
+  def charlist_parse(input), do: do_charlist_parse('', input) |> Enum.reverse()
 
-  def parse(input), do: do_parse('', input) |> Enum.reverse()
+  @doc """
+  Parses given input, returning next member of look and say sequence.
 
-  defp do_parse(acc, ''), do: acc
-  defp do_parse([char|[count|prev]], [char|next]) do
-    do_parse([char|[count + 1|prev]], next)
+  ## Examples
+
+      iex> LookSay.binary_parse("1")
+      "11"
+
+  """
+  @spec binary_parse(input :: String.t()) :: String.t()
+  def binary_parse(input), do: do_binary_parse("", input) |> String.reverse()
+
+  defp do_charlist_parse(acc, ''), do: acc
+  defp do_charlist_parse([char|[count|prev]], [char|next]) do
+    do_charlist_parse([char|[count + 1|prev]], next)
   end
-  defp do_parse(acc, [char|next]), do: do_parse([char|[?1|acc]], next)
+  defp do_charlist_parse(acc, [char|next]), do: do_charlist_parse([char|[?1|acc]], next)
+
+  defp do_binary_parse(acc, ""), do: acc
+  defp do_binary_parse(<<char::8, count::8, prev::binary>>, <<char::8, next::binary>>) do
+    do_binary_parse(<<char, count + 1>> <> prev, next)
+  end
+  defp do_binary_parse(acc, <<char::8, next::binary>>) do
+    do_binary_parse(<<char, ?1>> <> acc, next)
+  end
 end
