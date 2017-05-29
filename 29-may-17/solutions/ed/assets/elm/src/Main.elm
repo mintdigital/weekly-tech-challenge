5f9611d356770926de78f1f3bcc9a6f42dac2bf3
Ed Ellson <eellson@gmail.com>
2017-05-29 11:46:57 +0100

    Add initial Elm search field
    This hooks up a basic model, update and view for a search field.
    Currently this just render the input text into a p tag, as a proof of
    concept.

--- 29-may-17/solutions/ed/assets/elm/src/Main.elm
+++ 29-may-17/solutions/ed/assets/elm/src/Main.elm
@@ -1,6 +1,55 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 main =
-  Html.text "Hello, World!"
+  Html.program
+   { init = init
+   , view = view
+   , update = update
+   , subscriptions = subscriptions
+   }
+
+
+-- MODEL
+
+
+type alias Model =
+  { query : String
+  }
+
+init : (Model, Cmd Msg)
+init =
+  (Model "", Cmd.none)
+
+-- UPDATE
+
+
+type Msg = Search String
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Search query ->
+      (Model query, Cmd.none)
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+  [ input [type_ "text", placeholder "user", onInput Search ] []
+  , p [] [ text model.query ]
+  ]
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
