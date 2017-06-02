e3fa664c314423cd203d9d3f75f12edee2b4cb95
Simon Fletcher <simonfletcher0@gmail.com>
2017-06-02 17:54:30 +0100

    Vue approach

---
+++ 29-may-17/solutions/simon/main.js
@@ -0,0 +1,66 @@
+// Sub Components
+Vue.component('results-display', {
+  template: "#results-display",
+  props: ['results']
+});
+
+// Router Components
+const Search = Vue.component('github-search', {
+  template: "#github-search",
+  data () {
+    return {
+      search: '',
+      results: null
+    }
+  },
+  methods: {
+    fetchData: _.debounce(function() {
+      this.$http.get('https://api.github.com/search/users?q=' + this.search).then(response => {
+        this.results = response.data;
+      }, response => {
+        // HACK: using the 422 response to wipe the data
+        this.search = ''
+        this.results = null
+      });
+    }, 500)
+  }
+});
+
+const Profile = Vue.component('profile-display', {
+  template: "#profile-display",
+  data () {
+    return {
+      user: {}
+    }
+  },
+  mounted () {
+    this.fetchData();
+  },
+  methods: {
+    fetchData () {
+      this.$http.get(
+        'https://api.github.com/users/' + this.$route.params.username
+      ).then(response => {
+        this.user = response.data;
+      }, response => {
+        console.raise("Something went wrong!")
+      });
+    }
+  }
+})
+
+// Routes
+const routes = [
+  { path: '/:username', component: Profile },
+  { path: '/', component: Search }
+]
+
+const router = new VueRouter({
+  routes: routes
+})
+
+// Instantiate Vue
+const app = new Vue({
+  el: "#profiler",
+  router: router
+})
