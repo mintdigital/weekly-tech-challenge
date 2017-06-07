cb4b9ab79f3c67080d701559908ed286e65ce0be
Ed Ellson <eellson@gmail.com>
2017-06-07 11:58:24 +0100

    Tidy up TypeaheadChannel

--- 29-may-17/solutions/ed/lib/github_profiler/web/channels/typeahead_channel.ex
+++ 29-may-17/solutions/ed/lib/github_profiler/web/channels/typeahead_channel.ex
@@ -2,12 +2,8 @@ defmodule GithubProfiler.Web.TypeaheadChannel do
   use GithubProfiler.Web, :channel
 
   def join("typeahead:public", payload, socket) do
-    if authorized?(payload) do
-      send(self(), :after_join)
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+    send(self(), :after_join)
+    {:ok, socket}
   end
 
   def handle_info(:after_join, socket) do
@@ -15,33 +11,16 @@ defmodule GithubProfiler.Web.TypeaheadChannel do
     {:noreply, assign(socket, :producer_pid, producer_pid)}
   end
 
+  intercept ["search"]
   def handle_in("search", %{"query" => query}=payload, socket) do
     GithubProfiler.Query.update(socket.assigns.producer_pid, query)
 
     {:noreply, socket}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (typeahead:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
-  end
-
   def handle_out("search", payload, socket) do
     push socket, "results", payload
     {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
   end
 
   defp setup_genstage(socket) do
